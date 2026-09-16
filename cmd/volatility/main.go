1019a3244bfe3ac01f29f06ad9b7b65fb2f9b0eb
AntonK <pitshifer@gmail.com>
2026-09-16 18:39:18 +0700

    Add Dockerfile, docker-compose.yml, and main application code

---
+++ cmd/volatility/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+	slog.Info("shutting down...")
+}
